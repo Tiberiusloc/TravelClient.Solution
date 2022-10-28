40a7c0c9bc62daf43c32d61a221b1653b08b7388
Tiberius Lockett <tlockett680@gmail.com>
2022-10-28 09:06:00 -0700

    add mvp

---
+++ TravelClient/Controllers/ReviewsController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TravelClient.Models;
+
+namespace TravelClient.Controllers
+{
+  public class ReviewsController : Controller
+  {
+    [HttpGet("/reviews")]
+    public IActionResult Index()
+    {
+      var allReviews = Review.GetReviews();
+      return View(allReviews);
+    }
+    [HttpPost]
+    public IActionResult Index(Review review)
+    {
+      Review.Post(review);
+      return RedirectToAction("Index");
+    }
+    [HttpGet]
+    public IActionResult Details(int id)
+    {
+      var review = Review.GetDetails(id);
+      return View(review);
+    }
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+      var review = Review.GetDetails(id);
+      return View(review);
+    }
+    [HttpPost]
+    public IActionResult Edit(Review review)
+    {
+      // review.ReviewId = id;
+      Review.Put(review);
+      return RedirectToAction("Details", new { id = review.ReviewId});
+    }
+    public IActionResult Delete(int id)
+    {
+      Review.Delete(id);
+      return RedirectToAction("Index");
+    }
+  }
+}
